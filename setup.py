bd75237b285e2ef632abebf4fb7396bc11796908
Adam Schubert <adam.schubert@sg1-game.net>
2018-06-12 21:59:34 +0200

    New version 1.0.31

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ extra_files.extend(package_files('blacklist/static/bower_components/ekko-lightbo
 
 setup(
     name='blacklist',
-    version='1.0.30',
+    version='1.0.31',
     description='Blacklist',
     long_description=open('README.md').read(),
     author='Adam Schubert',
