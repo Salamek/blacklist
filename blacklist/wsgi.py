b5349c8747e2c11079836bd0e7e1e29fb524bea6
Adam Schubert <adam.schubert@sg1-game.net>
2020-08-18 17:32:35 +0200

    Delete wsgi.py

--- blacklist/wsgi.py
+++
@@ -1,8 +0,0 @@
-"""
-Bootstrap for use in uwsgi and so
-"""
-
-from blacklist.application import create_app, get_config
-
-config = get_config('blacklist.config.Production')
-app = create_app(config)
