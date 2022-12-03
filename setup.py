474dc48a3d3cd21d2663aa15730b1e9149c4a9e8
Adam Schubert <adam.schubert@sg1-game.net>
2022-12-03 02:22:23 +0100

    New version 1.0.47

--- setup.py
+++ setup.py
@@ -102,7 +102,7 @@ else:
 
 setup(
     name='blacklist',
-    version='1.0.46',
+    version='1.0.47',
     description='Blacklist',
     long_description=open('README.md').read(),
     author='Adam Schubert',
